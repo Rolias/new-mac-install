44668292dee7764d09902e1f43e672ea5908e84e
Tod Gentille <tod-gentille@Tods-Gray-Ghost.lan>
2018-03-09 01:20:34 -0800

    initial commit

---
+++ brew-tool-install.sh
@@ -0,0 +1,18 @@
+!#/bin/bash
+# brew cask search <name>
+# brew cask list -- find out what was installed
+# brew cask uninstall <name>
+brew cask install visual-studio-code
+brew cask install google-chrome
+brew tap cask room/fonts
+brew cask install font-fira-code
+brew cask install reaper
+brew cask install google-backup-and-sync
+brew cask install dropbox
+brew cask install lastpassbrew
+brew cask install blue-jeans
+brew cask install encryptme
+brew cask install slack
+brew cask install little-snitch
+brew cask install micro-snitch
+
