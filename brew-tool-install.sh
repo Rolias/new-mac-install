f7fcdda4616d44a9c89a6b2eea25b4bff0cdea91
Tod Gentille <tod-gentille@Tods-Gray-Ghost.local>
2018-04-22 14:38:16 -0700

    notes, added balsamiq, acrobat, anki, and atext

--- brew-tool-install.sh
+++ brew-tool-install.sh
@@ -1,8 +1,20 @@
 !#/bin/bash
+# README things to remember
 # brew cask search <name>
 # brew cask list -- find out what was installed
 # brew cask uninstall <name>
 # brew doctor <- checks the health of brew
+# ** Manual Install Required **
+#    Contour (The shuttle jog wheel settings)
+#    Slack from slack website because the brew install has limiations
+#    Bear (note taking app)
+
+# Some of the Mac App Store only items could be installed via a brew too called MAS
+# https://github.com/mas-cli/mas
+# brew install mas
+# then need to lookup the ids of each app and add lines
+# must give mas login credentials one time - see link
+# I'm not using it
 
 # developer tools
 brew install git
@@ -32,11 +44,18 @@ brew cask install dropbox
 brew cask install lastpassbrew
 brew cask install blue-jeans
 brew cask install encryptme
-brew cask install slack
+# brew cask install slack #install from their web site, this version currently has differences (like can't set download folder)
 brew cask install little-snitch
 brew cask install micro-snitch
 brew cask install bartender
 brew cask install grammarly
 brew cask install tunnelblick
 brew cask install macdown
+brew cask install adobe-air
+brew cask install balsamiq-mockups
+brew cask install adobe-acrobat-reader
+brew cask install anki   #spaced repetition learning software
+brew cask install atext
+
+
 
