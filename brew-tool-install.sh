203d04e2e729cffc3a4ace377bc179d234e5399a
Tod Gentille <tod-gentille@Tods-Gray-Ghost.lan>
2018-05-21 15:49:39 -0700

    Fix lastpass entry and add comments

--- brew-tool-install.sh
+++ brew-tool-install.sh
@@ -21,18 +21,19 @@ brew install git
 brew install node
 brew install nvm
 mkdir ~/.nvm
-#need heroku CLI for huh-bot
-brew install heroku
 
+brew install heroku                           # need heroku CLI for huh-bot
+
+# python
 brew install python3
 pip3 intall pandas
 python3 -m pip install jupyter
-# if working with google sheets in Python
-pip3 install pygsheets
+pip3 install pygsheets                         # if working with google sheets in Python
 
+# other languages
 brew install go
 brew install elixir
-brew install tree   #unix tree command that isn't there by default in OSX
+brew install tree                              # unix tree command that isn't there by default in OSX
 
 brew cask install visual-studio-code
 brew cask install google-chrome
@@ -41,24 +42,24 @@ brew cask install font-fira-code
 brew cask install reaper
 brew cask install google-backup-and-sync
 brew cask install dropbox
-brew cask install lastpassbrew
+brew cask install lastpass
 brew cask install blue-jeans
 brew cask install encryptme
 # brew cask install slack #install from their web site, this version currently has differences (like can't set download folder)
-brew cask install little-snitch
-brew cask install micro-snitch
-brew cask install bartender
-brew cask install grammarly
-brew cask install tunnelblick
+brew cask install little-snitch               # $
+brew cask install micro-snitch                # $
+brew cask install bartender                   # $
+brew cask install grammarly                   # $ but free for subset of features
+brew cask install tunnelblick                 # vpn
 brew cask install macdown
-brew cask install adobe-air
-brew cask install balsamiq-mockups
+brew cask install adobe-air                   # needed for balsamiq (otherwise seems like ancient technology)
+brew cask install balsamiq-mockups            # $ I refuse to give it up
 brew cask install adobe-acrobat-reader
-brew cask install anki   #spaced repetition learning software
+brew cask install anki                        # spaced repetition learning software
 brew cask install atext
-brew cask install sizeup #change mac monitor size
-brew cask install cleanmymac
-brew cask install screenflick   #screen recording software for the mac
+brew cask install sizeup                      # change mac monitor size - free alternatives available
+brew cask install cleanmymac                  # $
+brew cask install screenflick                 # screen recording software for the mac
 
 
 
